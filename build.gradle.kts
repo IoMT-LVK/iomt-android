25fa55b40b2132989fe53bf2fc987ebde8054592
Alexander Frolov <lxnfrolov@gmail.com>
2023-04-09 03:50:47 +0300

    Added dokka (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,11 @@ buildscript {
     dependencies {
         classpath ("com.android.tools.build:gradle:7.2.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin") {
+            version {
+                strictly("1.8.10")
+            }
+        }
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
