993b76004eff3f5c65a8a814cbc181b64422af85
sanyavertolet <lxnfrolov@gmail.com>
2023-04-11 22:57:11 +0300

    Added CredentialManager support

--- app/src/main/kotlin/com/iomt/android/dto/Credentials.kt
+++ app/src/main/kotlin/com/iomt/android/dto/Credentials.kt
@@ -1,5 +1,7 @@
 package com.iomt.android.dto
 
+import androidx.credentials.GetCredentialResponse
+import androidx.credentials.PasswordCredential
 import kotlinx.serialization.Serializable
 
 /**
@@ -11,3 +13,14 @@ data class Credentials(
     val login: String,
     val password: String,
 )
+
+/**
+ * @return [Credentials] from [GetCredentialResponse]
+ */
+fun GetCredentialResponse.toCredentials(): Credentials? = when (credential) {
+    is PasswordCredential -> {
+        val passwordCredentials = credential as PasswordCredential
+        Credentials(passwordCredentials.id, passwordCredentials.password)
+    }
+    else -> null
+}
